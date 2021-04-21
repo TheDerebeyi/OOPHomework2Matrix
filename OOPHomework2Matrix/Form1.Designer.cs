c602564c20ae8e0808233c7417984af4689c2ac7
thederebeyi <rehber.tadinda1@gmail.com>
2021-04-21 13:05:31 +0300

    Add project files.

---
+++ OOPHomework2Matrix/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace OOPHomework2Matrix
+{
+    partial class MatrixCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.buttonToplama = new System.Windows.Forms.Button();
+            this.buttonÇarpma = new System.Windows.Forms.Button();
+            this.textBoxSonuc = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(23, 12);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(51, 22);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(80, 12);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(48, 22);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(713, 12);
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(51, 22);
+            this.numericUpDown3.TabIndex = 6;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(770, 12);
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(51, 22);
+            this.numericUpDown4.TabIndex = 7;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // buttonToplama
+            // 
+            this.buttonToplama.Location = new System.Drawing.Point(598, 99);
+            this.buttonToplama.Name = "buttonToplama";
+            this.buttonToplama.Size = new System.Drawing.Size(36, 30);
+            this.buttonToplama.TabIndex = 8;
+            this.buttonToplama.Text = "+";
+            this.buttonToplama.UseVisualStyleBackColor = true;
+            // 
+            // buttonÇarpma
+            // 
+            this.buttonÇarpma.Location = new System.Drawing.Point(598, 146);
+            this.buttonÇarpma.Name = "buttonÇarpma";
+            this.buttonÇarpma.Size = new System.Drawing.Size(36, 30);
+            this.buttonÇarpma.TabIndex = 9;
+            this.buttonÇarpma.Text = "X";
+            this.buttonÇarpma.UseVisualStyleBackColor = true;
+            // 
+            // textBoxSonuc
+            // 
+            this.textBoxSonuc.Location = new System.Drawing.Point(165, 377);
+            this.textBoxSonuc.Multiline = true;
+            this.textBoxSonuc.Name = "textBoxSonuc";
+            this.textBoxSonuc.ReadOnly = true;
+            this.textBoxSonuc.Size = new System.Drawing.Size(369, 157);
+            this.textBoxSonuc.TabIndex = 10;
+            // 
+            // MatrixCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1339, 576);
+            this.Controls.Add(this.textBoxSonuc);
+            this.Controls.Add(this.buttonÇarpma);
+            this.Controls.Add(this.buttonToplama);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "MatrixCalculator";
+            this.Text = "Matrix Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Button buttonToplama;
+        private System.Windows.Forms.Button buttonÇarpma;
+        private System.Windows.Forms.TextBox textBoxSonuc;
+    }
+}
+
---
+++ OOPHomework2Matrix/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mime;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOPHomework2Matrix
+{
+    public partial class MatrixCalculator : Form
+    {
+        private List<TextBox> matris1 = new List<TextBox>();
+        private List<TextBox> matris2 = new List<TextBox>();
+
+        public MatrixCalculator()
+        {
+            InitializeComponent();
+            numericUpDown1.ValueChanged += new System.EventHandler(NumericUpDown1_ValueChanged);
+            numericUpDown2.ValueChanged += new System.EventHandler(NumericUpDown2_ValueChanged);
+            numericUpDown3.ValueChanged += new System.EventHandler(NumericUpDown3_ValueChanged);
+            numericUpDown4.ValueChanged += new System.EventHandler(NumericUpDown4_ValueChanged);
+
+            buttonToplama.Click += new System.EventHandler(ButtonToplama_Click);
+
+            for (int i = 0; i < numericUpDown1.Value; i++)
+            {
+                for (int j = 0; j < numericUpDown2.Value; j++)
+                {
+                    matris1.Add(new TextBox());
+                    matris1[i * (int)numericUpDown2.Value + j].Left = (j+1) * 40;
+                    matris1[i * (int)numericUpDown2.Value + j].Top = (i+1) * 40;
+                    matris1[i * (int)numericUpDown2.Value + j].Width = 25;
+                }
+            }
+
+            for (int i = 0; i < numericUpDown3.Value; i++)
+            {
+                for (int j = 0; j < numericUpDown4.Value; j++)
+                {
+                    matris2.Add(new TextBox());
+                    matris2[i * (int)numericUpDown4.Value + j].Left = this.Width/2 + (j + 1) * 40;
+                    matris2[i * (int)numericUpDown4.Value + j].Top = (i + 1) * 40;
+                    matris2[i * (int)numericUpDown4.Value + j].Width = 25;
+                }
+            }
+
+            foreach (var eleman in matris1)
+            {
+                this.Controls.Add(eleman);
+            }
+
+            foreach (var eleman in matris2)
+            {
+                this.Controls.Add(eleman);
+            }
+
+        }
+
+        private void NumericUpDown1_ValueChanged(Object sender, EventArgs e)
+        {
+            foreach (var eleman in matris1)
+            {
+                this.Controls.Remove(eleman);
+            }
+
+            matris1.Clear();
+
+            for (int i = 0; i < numericUpDown1.Value; i++)
+            {
+                for (int j = 0; j < numericUpDown2.Value; j++)
+                {
+                    matris1.Add(new TextBox());
+                    matris1[i * (int)numericUpDown2.Value + j].Left = (j + 1) * 40;
+                    matris1[i * (int)numericUpDown2.Value + j].Top = (i + 1) * 40;
+                    matris1[i * (int)numericUpDown2.Value + j].Width = 25;
+                }
+            }
+
+            foreach (var eleman in matris1)
+            {
+                this.Controls.Add(eleman);
+            }
+        }
+
+        private void NumericUpDown2_ValueChanged(Object sender, EventArgs e)
+        {
+            foreach (var eleman in matris1)
+            {
+                this.Controls.Remove(eleman);
+            }
+
+            matris1.Clear();
+
+            for (int i = 0; i < numericUpDown1.Value; i++)
+            {
+                for (int j = 0; j < numericUpDown2.Value; j++)
+                {
+                    matris1.Add(new TextBox());
+                    matris1[i * (int)numericUpDown2.Value + j].Left = (j + 1) * 40;
+                    matris1[i * (int) numericUpDown2.Value + j].Top = (i + 1) * 40;
+                    matris1[i * (int)numericUpDown2.Value + j].Width = 25;
+                }
+            }
+
+            foreach (var eleman in matris1)
+            {
+                this.Controls.Add(eleman);
+            }
+        }
+
+        private void NumericUpDown3_ValueChanged(Object sender, EventArgs e)
+        {
+            foreach (var eleman in matris2)
+            {
+                this.Controls.Remove(eleman);
+            }
+
+            matris2.Clear();
+
+            for (int i = 0; i < numericUpDown3.Value; i++)
+            {
+                for (int j = 0; j < numericUpDown4.Value; j++)
+                {
+                    matris2.Add(new TextBox());
+                    matris2[i * (int)numericUpDown4.Value + j].Left = this.Width / 2 + (j + 1) * 40;
+                    matris2[i * (int)numericUpDown4.Value + j].Top = (i + 1) * 40;
+                    matris2[i * (int)numericUpDown4.Value + j].Width = 25;
+                }
+            }
+
+            foreach (var eleman in matris2)
+            {
+                this.Controls.Add(eleman);
+            }
+        }
+
+        private void NumericUpDown4_ValueChanged(Object sender, EventArgs e)
+        {
+            foreach (var eleman in matris2)
+            {
+                this.Controls.Remove(eleman);
+            }
+
+            matris2.Clear();
+
+            for (int i = 0; i < numericUpDown3.Value; i++)
+            {
+                for (int j = 0; j < numericUpDown4.Value; j++)
+                {
+                    matris2.Add(new TextBox());
+                    matris2[i * (int)numericUpDown4.Value + j].Left = this.Width / 2 + (j + 1) * 40;
+                    matris2[i * (int)numericUpDown4.Value + j].Top = (i + 1) * 40;
+                    matris2[i * (int)numericUpDown4.Value + j].Width = 25;
+                }
+            }
+
+            foreach (var eleman in matris2)
+            {
+                this.Controls.Add(eleman);
+            }
+        }
+
+        private void ButtonToplama_Click(Object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value != numericUpDown3.Value || numericUpDown2.Value != numericUpDown4.Value)
+            {
+                textBoxSonuc.Text =
+                    "HATA! Girilen matrislerin tipleri eşit değil! Lütfen aynı tipte matrisler giriniz...";
+                return;
+            }
+
+            if (!IntKontrol())
+            {
+                return;
+            }
+
+            var sonucMatris = new List<int>();
+
+            for (int i = 0; i < matris1.Count; i++)
+            {
+                int intDegisken1, intDegisken2;
+                int.TryParse(matris1[i].Text, out intDegisken1);
+                int.TryParse(matris2[i].Text, out intDegisken2);
+
+                sonucMatris.Add((intDegisken1 + intDegisken2));
+            }
+
+            textBoxSonuc.Clear();
+
+            for (int i = 0; i < sonucMatris.Count; i++)
+            {
+                if (i % (int)numericUpDown1.Value == 0)
+                {
+                    textBoxSonuc.Text += "\r\n";
+                }
+
+                textBoxSonuc.Text += sonucMatris[i] + " ";
+            }
+        }
+
+        private bool IntKontrol()
+        {
+            int intDegisken;
+            bool hataVarMi1 = false, hataVarMi2 = false;
+
+            foreach (var eleman in matris1)
+            {
+                if (!int.TryParse(eleman.Text, out intDegisken))
+                {
+                    hataVarMi1 = true;
+                }
+            }
+
+            foreach (var eleman in matris2)
+            {
+                if (!int.TryParse(eleman.Text, out intDegisken))
+                {
+                    hataVarMi2 = true;
+                }
+            }
+
+            if (hataVarMi1)
+            {
+                textBoxSonuc.Text =
+                    "HATA! Birinci matriste girilen elemanlardan birisi hatalı, sadece tam sayılar giriniz...";
+                return false;
+            }
+
+            if (hataVarMi2)
+            {
+                textBoxSonuc.Text =
+                    "HATA! İkinci matriste girilen elemanlardan birisi hatalı, sadece tam sayılar giriniz...";
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
